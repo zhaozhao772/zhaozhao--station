1d301495661fc774122aaed633658dd99bed5168
zhaozhao772 <242589325@qq.com>
2026-09-25 19:17:07 +0800

    阅读模块图标替换：📖改为黑色笔记本+粉色蝴蝶结+爱心图片（去水印抠图），SW v32

--- js/app.js
+++ js/app.js
@@ -17,7 +17,7 @@ const App = {
     { id: 'home',       name: '今日工作台', icon: '<img class="nav-img-icon" src="img/home_tv.png" alt="今日工作台">', privacy: false },
     { id: 'timer',      name: '工作与专注计时', icon: '<img class="nav-img-icon" src="img/timer_alarm.png" alt="工作与专注计时">', privacy: false },
     { id: 'projects',   name: '项目与内容管理', icon: '<img class="nav-img-icon" src="img/timer_cd.png" alt="项目与内容管理">', privacy: false },
-    { id: 'reading',    name: '阅读模块', icon: '📖', privacy: false },
+    { id: 'reading',    name: '阅读模块', icon: '<img class="nav-img-icon" src="img/reading_note.png" alt="阅读模块">', privacy: false },
     { id: 'workout',    name: '锻炼板块', icon: '🏃🏻‍♀️', privacy: false },
     { id: 'emotion',    name: '情绪天气监测', icon: '<img class="nav-img-icon" src="img/emotion_jar.png" alt="情绪天气监测">', privacy: false },
     { id: 'emotion-analysis', name: '情绪循环分析', icon: '🧠', privacy: false },
